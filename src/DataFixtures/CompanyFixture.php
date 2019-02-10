1151961be265449384bfd3c1fb59db9cb040b09e
Tiphaine Chartrain <tiphaine.chartrain@gmail.com>
2019-02-10 17:22:20 +0100

    resolve #12 Improve company model with properties, display the company list and style

--- src/DataFixtures/CompanyFixture.php
+++ src/DataFixtures/CompanyFixture.php
@@ -14,6 +14,9 @@ class CompanyFixture extends Fixture
         foreach ($companiesData as $key => $companyData) {
             $company = new Company();
             $company->setName($companyData[0]);
+            $company->setType($companyData[1]);
+            $company->setGrading($companyData[2]);
+            $company->setComment($companyData[3]);
             $manager->persist($company);
             $this->addReference($key, $company);
         }
@@ -23,9 +26,11 @@ class CompanyFixture extends Fixture
     private function getCompaniesData(): array
     {
         return [
-            'company-acme' => ['Acme'],
-            'company-google' => ['Google'],
-            'company-ibm' => ['IBM'],
+            'company-acme' => ['Acme','Customer',null,null],
+            'company-google' => ['Google','Software vendor', 5, null],
+            'company-ibm' => ['IBM','Software vendor', 4, null],
+            'company-abba' => ['Abba','Recruitment company', 3, null],
+            'company-sp' => ['Sp','Solution provider', 0, null],
         ];
     }
 }
--- src/Entity/Company.php
+++ src/Entity/Company.php
@@ -21,6 +21,21 @@ class Company
      */
     private $name;
 
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $grading;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $comment;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -34,6 +49,42 @@ class Company
     public function setName(string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getGrading(): ?int
+    {
+        return $this->grading;
+    }
+
+    public function setGrading(?int $grading): self
+    {
+        $this->grading = $grading;
+
+        return $this;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(?string $comment): self
+    {
+        $this->comment = $comment;
 
         return $this;
     }
